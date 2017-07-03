83bcb65fe5b8f1acf24e8b5f5e31d70670437460
kofes <turnever@gmail.com>
2017-07-04 07:21:39 +1000

    TSP LP realization

--- main.cpp
+++ main.cpp
@@ -1,25 +1,172 @@
-#include "inc/Graph.hpp"
+#include <iostream>
 #include <fstream>
+#include <vector>
+#include <limits>
+#include <cmath>
+#include "qsopt.h"
 
-int main() {
-  size_t count;
-  std::ifstream fin("input0.txt");
-  std::ofstream fout("output.txt");
-  fin >> count;
-  Graph graph(count);
-  for (int i = 0; i < count; ++i)
-    for (int j = 0; j < count; ++j) {
-      int cost;
-      fin >> cost;
-      if (cost == -1) cost = INT_MAX;
-      graph.add_edge(i, j, cost);
+static std::vector< std::vector<double> > matrix;
+size_t size;
+std::ifstream input("input.txt");
+std::ofstream output("output.txt");
+const double max_value = 1e+7;
+const double eps = 1e-4;
+
+class TSP {
+public:
+  TSP();
+  ~TSP() {QSfree(qSprob);}
+  void solve();
+  double get_min() { return min; }
+  void print();
+private:
+  QSprob qSprob;
+  size_t ncols;
+  size_t nrows;
+  double min = max_value;
+};
+
+TSP::TSP() {
+  ncols = size*size;
+  nrows = 2*size;
+  std::vector<int> cmatcnt(ncols, 2);
+  std::vector<int> cmatbeg(ncols);
+  for (size_t i = 0; i < ncols; ++i)
+    cmatbeg[i] = 2*i;
+  std::vector<int> cmatind;
+  for (size_t i = 0; i < ncols; ++i)
+    for (size_t j = 0; j < size; ++j) {
+      cmatind.push_back(i);
+      cmatind.push_back(size+j);
     }
-  auto res = graph.find_path();
-  int cost = 0;
-  for (auto elem : res) {
-    cost += graph[elem.first][elem.second];
-    fout << elem.first+1 << ' ' << elem.second+1 << std::endl;
+  std::vector<double> cmatval(ncols*nrows, 1.0);
+  std::vector<double> obj;
+  for (size_t i = 0; i < size; ++i)
+    for (size_t j = 0; j < size; ++j)
+      obj.push_back(matrix[i][j]);
+  std::vector<double> rhs(nrows, 1.0);
+  std::vector<char> sense(nrows, 'E');
+  std::vector<double> lower(ncols, 0.0);
+  std::vector<double> upper(ncols, 1.0);
+  for (size_t i = 0; i < size; ++i)
+    upper[i*size+i] = 0;
+  qSprob = QSload_prob("TSP", ncols, nrows, cmatcnt.data(), cmatbeg.data(),
+                       cmatind.data(), cmatval.data(), QS_MIN, obj.data(),
+                       rhs.data(), sense.data(), lower.data(), upper.data(),
+                       NULL, NULL);
+  if (qSprob == NULL)
+    throw "Can't load LP\n";
+  QSset_param(qSprob, QS_PARAM_PRIMAL_PRICING, QS_PRICE_PDANTZIG);
+}
+
+void TSP::solve() {
+  int indicator;
+  int status;
+  indicator = QSopt_primal(qSprob, &status);
+  if (indicator) {
+    std::cerr << "QSopt_dual failed with return's: " << indicator << std::endl;
+    return;
   }
-  fout << "COST: " << cost << std::endl;
+  if (status != QS_LP_OPTIMAL) {
+    std::cerr << "LP could not be solved, status : " << status << std::endl;
+    return;
+  }
+
+  double value;
+  std::vector<double> x(ncols, -1.0);
+  QSget_solution(qSprob, &value, x.data(), NULL, NULL, NULL);
+  if (value >= min) return;
+
+  bool allInt = true;
+  std::vector<int> path(size);
+  for (int i = 0; i < x.size(); ++i) {
+    if (!(std::fabs(x[i]) < eps || std::fabs(x[i] - 1) < eps)) {
+      allInt = false;
+      QSchange_bound(qSprob, i, 'L', 1);
+      QSchange_bound(qSprob, i, 'U', 1);
+      solve();
+      QSchange_bound(qSprob, i, 'L', 0);
+      QSchange_bound(qSprob, i, 'U', 0);
+      solve();
+      QSchange_bound(qSprob, i, 'L', 0);
+      QSchange_bound(qSprob, i, 'U', 1);
+      break;
+    } else if (std::fabs(x[i] - 1) < eps)
+      path[i / size] = i % size;
+  }
+
+  if (allInt) {
+    std::vector<bool> visited(size, false);
+    int countChecked = 0;
+    bool cycle = false;
+    std::vector<std::vector<int>> cycles(size, std::vector<int>());
+    int ind = -1;
+    for (int i = 0; i < size; ++i)
+      if (!visited[i]) {
+        ++ind;
+        int v = i;
+        cycles[ind].push_back(v);
+        while (true) {
+          visited[v] = true;
+          ++countChecked;
+          v = path[v];
+          cycles[ind].push_back(v);
+          if (visited[v] && countChecked != size) {
+            cycle = true;
+            break;
+          } else if (visited[v])
+            break;
+        }
+      }
+
+    if (cycle) {
+      for (int j = 0; j <= ind; ++j) {
+        int indicator;
+        std::vector<double> rmatval(cycles[j].size() - 1, 1.0);
+        std::vector<int> rmatind;
+        for (int i = 0; i < cycles[j].size() - 1; ++i)
+          rmatind.push_back(cycles[j][i] * size + cycles[j][i + 1]);
+        indicator = QSadd_row(qSprob, (int) rmatind.size(), rmatind.data(),
+                         rmatval.data(), cycles[j].size() - 2, 'L', NULL);
+        if (indicator)
+          std::cout << "QSadd_row failed with return's: " << indicator << std::endl;
+      }
+      solve();
+    } else min = std::min(value, min);
+  }
+  return;
+}
+
+void TSP::print() {
+  double value;
+  std::vector<double> x(ncols);
+  QSget_solution(qSprob, &value, x.data(), NULL, NULL, NULL);
+  std::cout << value << std::endl;
+  for (size_t i = 0; i < size; ++i, std::cout << std::endl)
+    for (size_t j = 0; j < size; ++j)
+      std::cout << x[i*size+j] << '\t';
+}
+
+int main () {
+  input >> size;
+  matrix.resize(size);
+  for (size_t i = 0; i < size; ++i)
+    matrix[i].resize(size, 0);
+  for (size_t i = 0; i < size; ++i)
+    for (size_t j = 0; j < size; ++j) {
+      if (i==j) matrix[i][j] = max_value;
+      double cost;
+      input >> cost;
+      matrix[i][j] = cost;
+    }
+  try {
+    TSP tsp;
+    tsp.solve();
+    std::cout << tsp.get_min() << std::endl;
+    // if (tsp.get_min() != max_value)
+    //   tsp.print();
+  } catch (std::string& err) {
+    std::cout << err << std::endl;
+  }
   return 0;
 }
